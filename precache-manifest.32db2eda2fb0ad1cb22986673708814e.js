f48f70d0b74298398a76c1fffcb795dc7ea4a2ae
Thanh Tùng <thanhtung.uet@gmail.com>
2018-10-27 12:11:41 +0700

    First commit

---
+++ precache-manifest.32db2eda2fb0ad1cb22986673708814e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b57c6a5567c9ef09913983f82904faa",
+    "url": "/static/media/CoreUI-Icons-Linear-Free.4b57c6a5.svg"
+  },
+  {
+    "revision": "1bc3764815bb391a16771c42c6e460ec",
+    "url": "/static/media/CoreUI-Icons-Linear-Free.1bc37648.ttf"
+  },
+  {
+    "revision": "089ab3c11c572362d088083c561cfa55",
+    "url": "/static/media/CoreUI-Icons-Linear-Free.089ab3c1.eot"
+  },
+  {
+    "revision": "0087dce419a7376eb06c837f740d31eb",
+    "url": "/static/media/CoreUI-Icons-Linear-Free.0087dce4.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "61bf4e2236b7f386cde4",
+    "url": "/static/js/main.61bf4e22.chunk.js"
+  },
+  {
+    "revision": "8b3f48a7b75073f2acd0",
+    "url": "/static/js/1.8b3f48a7.chunk.js"
+  },
+  {
+    "revision": "61bf4e2236b7f386cde4",
+    "url": "/static/css/main.551f1068.chunk.css"
+  },
+  {
+    "revision": "8b3f48a7b75073f2acd0",
+    "url": "/static/css/1.355af06f.chunk.css"
+  },
+  {
+    "revision": "b5f23822b2719daf614d596053794b67",
+    "url": "/index.html"
+  }
+];
